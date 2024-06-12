8021f9e310943e136b45f9fbe595cab9a42a625f
Pin-Ching Chen <p.ching0811@gmail.com>
2024-06-12 18:36:45 +0800

    Neovim Configuration: Remove redundant large parentheses in lua/plugins/luasnip.lua, lua/plugins/snippets.lua, and lua/plugins/which-key.lua

--- .config/nvim/lua/plugins/snippets.lua
+++ .config/nvim/lua/plugins/snippets.lua
@@ -1,9 +1,7 @@
 return
 {
-    {
-        "rafamadriz/friendly-snippets",
-        config = function()
-            require("luasnip.loaders.from_vscode").lazy_load()
-        end,
-    }
+    "rafamadriz/friendly-snippets",
+    config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+    end,
 }
