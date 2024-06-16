020ad08fd4264596c8a117b1959afc7ecc20ee40
Pin-Ching Chen <p.ching0811@gmail.com>
2024-06-16 14:54:03 +0800

    Neovim: Add comments to lua/plugins/linting.lua

--- .config/nvim/lua/plugins/linting.lua
+++ .config/nvim/lua/plugins/linting.lua
@@ -1,3 +1,5 @@
+-- Plugin `nvim-lint` highlights syntax errors, style issues, and potential bugs in real-time.
+
 return {
     "mfussenegger/nvim-lint",
     event = {
@@ -14,7 +16,7 @@ return {
 
         local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
             group = lint_augroup,
             callback = function()
                 lint.try_lint()
