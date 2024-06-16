aab9f22be01041aebe9edbc39b56066acd6d5f14
Pin-Ching Chen <p.ching0811@gmail.com>
2024-06-16 15:28:32 +0800

    Neovim: Add global variables for language servers and language parsers in `lua/settings.lua`

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -1,7 +1,43 @@
 return {
-  colorscheme = "nord" -- Colorschemes:
-                                     -- nord
-                                     -- kanagawa
-                                     -- tokyonight
+    colorscheme = "nord", --[[
+                                Colorschemes:
+                                - nord
+                                - kanagawa
+                                - tokyonight
+                            ]]
+    language_servers = {
+        "lua_ls",
+        "clangd",
+        "html",
+        "tsserver",
+        "golangci_lint_ls",
+        "gopls",
+    },
+    language_parsers = {
+        "bash",
+        "c",
+        "diff",
+        "html",
+        "go",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "printf",
+        "python",
+        "query",
+        "regex",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
+    },
 }
-
