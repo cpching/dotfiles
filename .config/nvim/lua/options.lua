d453b2b02dca554b48c10e489df717e8bc1cb78d
Pin-Ching Chen <p.ching0811@gmail.com>
2024-06-16 15:10:24 +0800

    Neovim: Update `lua/options.lua` for relativenumber, statuscolumn, and signcolumn
    - Added relativenumber setting.
    - Modified statuscolumn to adjust the display of status information.
    - Adjusted signcolumn for better visual alignment and clarity.

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,6 +1,7 @@
 local options =
 {
 	number = true,
+    relativenumber = true,
 	smartindent = true,
 	smartcase = true,
 	termguicolors = true,
@@ -14,6 +15,8 @@ local options =
 	tabstop = 4,
 	hidden = false,
 	mouse="i",
+    statuscolumn = "%s%2r  %2l ", -- ▎
+    signcolumn = "yes"
 }
 
 for key, value in pairs(options) do
